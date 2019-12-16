bfc048b276a382da2286dae3bbc3af1d48437496
Annie Huang <annie.hn@gmail.com>
2019-12-17 08:58:05 +1100

    10: Pointers and the reason we use pointers

---
+++ 10_pointers/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// The reason you will use pointer is that e.g. if your data stored in the memory is huge
+	// It will be better for performance to just pass the memory address in rather than data value
+	
+	a := 5
+	b := &a	// b is the pointer memory address
+
+	fmt.Println(a, b)
+	fmt.Printf("%T\n", a)	// int
+	fmt.Printf("%T\n", b)	// *int
+
+	// Use * to read val from address
+	fmt.Println(*b)
+	fmt.Println(*&a)
+
+	// Change val with pointer
+	*b = 10
+	fmt.Println(a)
+}
