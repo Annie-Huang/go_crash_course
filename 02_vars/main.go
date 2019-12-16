4c59658df08620dea2f70010aafe61f5ec77af35
Annie Huang <annie.hn@gmail.com>
2019-12-17 06:57:45 +1100

    How to do var, const and shorthanded.

--- 02_vars/main.go
+++ 02_vars/main.go
@@ -1,5 +1,10 @@
 package main
 
+import "fmt"
+
+//var name = "Brad"
+
+
 func main() {
 	// MAIN TYPES
 	// string
@@ -11,4 +16,41 @@ func main() {
 	// rune - alias for int32
 	// float32 float64
 	// complex64 complex128
+
+	// Using var
+	//var name string = "Brad"	// Don't need to explicit the type as it's already inferred
+	//var name = "Brad"
+	//var age int = 37	// Same as above
+	var age int32 = 37
+	const isCool = true
+	var size float32 = 2.3
+
+	// Shorthand
+	//name := "Brad"
+	//size := 1.3	// default type is float64
+	//email := "brad@gmail.com"
+
+	// Assigned multiple values in one line
+	name, email := "Brad", "brad@gmail.com"
+
+	fmt.Println(name, age, isCool, email)
+	fmt.Printf("%T\n", size)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
